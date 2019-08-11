32ca849dee71daf96bd96c70a2bfe49bc0f3a603
Josh Samuels <joshsamuels04@gmail.com>
2019-08-11 16:27:26 -0700

    Included support for reading directories recursively

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"path/filepath"
 	"regexp"
 )
 
@@ -27,19 +28,95 @@ func main() {
 	args := os.Args[1:]
 
 	if len(args) == 0 {
-		fmt.Printf("At least one filename must be supplied as an argument")
+		fmt.Printf("At least one filename must be supplied as an argument\n")
 		os.Exit(1)
 	}
 
-	licenses := make(map[License][]string)
+	var licenses map[License][]string
 
 	for _, filename := range args {
-		newLicenses := getLicensesForFile(filename)
-
+		newLicenses := getLicenses(filename)
 		licenses = mergeMaps(licenses, newLicenses)
 	}
 
 	prettyPrintLicenses(licenses)
+}
+
+func getLicenses(filename string) map[License][]string {
+	isDirectory, err := isDir(filename)
+	if err != nil {
+		fmt.Printf("Error checking if %s is a directory. Error was %v\n", filename, err)
+		return nil
+	}
+
+	var licenses map[License][]string
+
+	if isDirectory {
+		newLicenses := getLicensesFromDir(filename)
+		licenses = mergeMaps(licenses, newLicenses)
+	} else {
+		newLicenses := getLicensesFromFile(filename)
+		licenses = mergeMaps(licenses, newLicenses)
+	}
+
+	return licenses
+}
+
+func getLicensesFromFile(filename string) map[License][]string {
+	licenses := make(map[License][]string)
+
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Printf("Error opening file %s. Error: %v\n", "go.mod", err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := stripNewline(scanner.Text())
+
+		repos, err := getGithubRepos(line)
+		if err != nil {
+			fmt.Printf("Error getting github repos %v\n", err)
+		}
+
+		for _, repo := range repos {
+			license, err := getGithubLicense(repo)
+			if err != nil {
+				fmt.Printf("Error getting license info %v\n", err)
+			}
+
+			licenses[license] = append(licenses[license], repo)
+		}
+	}
+
+	return licenses
+}
+
+func getLicensesFromDir(dir string) map[License][]string {
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		fmt.Printf("Error reading the directory %s. Error was %v\n", dir, err)
+		return nil
+	}
+
+	var licenses map[License][]string
+
+	for _, file := range files {
+		newLicenses := getLicenses(filepath.Join(dir, file.Name()))
+		licenses = mergeMaps(licenses, newLicenses)
+	}
+
+	return licenses
+}
+
+func isDir(filename string) (bool, error) {
+	fi, err := os.Stat(filename)
+	if err != nil {
+		return false, err
+	}
+
+	return fi.IsDir(), nil
 }
 
 func getGithubLicense(repo string) (License, error) {
@@ -94,37 +171,6 @@ func getGithubRepos(text string) ([]string, error) {
 	return regex.FindAllString(text, -1), nil
 }
 
-func getLicensesForFile(filename string) map[License][]string {
-	licenses := make(map[License][]string)
-
-	file, err := os.Open(filename)
-	if err != nil {
-		fmt.Printf("Error opening file %s. Error: %v\n", "go.mod", err)
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := stripNewline(scanner.Text())
-
-		repos, err := getGithubRepos(line)
-		if err != nil {
-			fmt.Printf("Error getting github repos %v\n", err)
-		}
-
-		for _, repo := range repos {
-			license, err := getGithubLicense(repo)
-			if err != nil {
-				fmt.Printf("Error getting license info %v", err)
-			}
-
-			licenses[license] = append(licenses[license], repo)
-		}
-	}
-
-	return licenses
-}
-
 func (license *License) print() {
 	fmt.Printf("Name: %s | Key: %s | URL: %s\n", license.Name, license.Key, license.URL)
 }
@@ -155,6 +201,18 @@ func printArr(prompt string, arr []string) {
 }
 
 func mergeMaps(map1 map[License][]string, map2 map[License][]string) map[License][]string {
+	if map1 == nil && map2 == nil {
+		return make(map[License][]string)
+	}
+
+	if map1 == nil {
+		return map2
+	}
+
+	if map2 == nil {
+		return map1
+	}
+
 	merged := make(map[License][]string)
 
 	// copys all the values from map1 into the new map
@@ -168,8 +226,32 @@ func mergeMaps(map1 map[License][]string, map2 map[License][]string) map[License
 			continue
 		}
 
-		merged[key] = append(merged[key], map2[key]...)
+		merged[key] = appendUnique(merged[key], map2[key]...)
 	}
 
 	return merged
+}
+
+func appendUnique(currentStrings []string, newStrings ...string) []string {
+	var newArrStrings []string
+
+	for _, currentString := range currentStrings {
+		newArrStrings = append(newArrStrings, currentString)
+	}
+
+	for _, newString := range newStrings {
+		found := false
+		for _, currentString := range currentStrings {
+			if newString == currentString {
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			newArrStrings = append(newArrStrings, newString)
+		}
+	}
+
+	return newArrStrings
 }
